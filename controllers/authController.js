0512643c0e1679a9ce07588b33746e8d102978a5
p7 <vmixp7@gmail.com>
2026-01-17 16:00:55 +0800

    edit usermodel

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,35 @@
-import { verifyUserPassword } from "../models/UserModel.js";
+import { verifyUserPassword } from "../models/userModel.js";
 
 export const showLogin = (req, res) => {
-    // 如果已登入就直接導向後台
-    if (req.session.user) {
-        return res.redirect("/admin");
-    }
-    res.render("login", { title: "登入", message: null });
+  // 如果已登入就直接導向後台
+  if (req.session.user) {
+    return res.redirect("/admin");
+  }
+  res.render("login", { title: "登入", message: null });
 };
 
 export const login = (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    verifyUserPassword(username, password, (err, user) => {
-        if (err) {
-            return res.render("login", { title: "登入", message: "伺服器錯誤，請稍後再試" });
-        }
-        if (!user) {
-            return res.render("login", { title: "登入", message: "帳號或密碼錯誤" });
-        }
+  verifyUserPassword(username, password, (err, user) => {
+    if (err) {
+      return res.render("login", {
+        title: "登入",
+        message: "伺服器錯誤，請稍後再試",
+      });
+    }
+    if (!user) {
+      return res.render("login", { title: "登入", message: "帳號或密碼錯誤" });
+    }
 
-        // 登入成功後寫入 session
-        req.session.user = user;
-        res.redirect("/dashboard");
-    });
+    // 登入成功後寫入 session
+    req.session.user = user;
+    res.redirect("/dashboard");
+  });
 };
 
 export const logout = (req, res) => {
-    req.session.destroy(() => {
-        res.redirect("/");
-    });
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
 };
