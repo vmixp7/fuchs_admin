f73a6f7d13925dbd50d3231bdcf86852e1e67deb
p7 <vmixp7@gmail.com>
2026-01-18 23:37:03 +0800

    edit ssl

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,3 @@
-const BASE_URL = "fuchslube.com.tw/admin";
+const BASE_URL = "https://fuchslube.com.tw/admin";
 // const BASE_URL = "http://127.0.0.1:5000";
 const FORMOSA_URL = "https://www.formosalube.com.tw";
