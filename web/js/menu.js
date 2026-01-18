2ba1a2781ee397a83fbf6756e87bcdea20a0dade
p7 <vmixp7@gmail.com>
2026-01-18 20:22:22 +0800

    edit 0112

--- web/js/menu.js
+++ web/js/menu.js
@@ -1,39 +1,362 @@
-// header
-// $(window).scroll(function () {
-//   if ($(this).scrollTop() > 100) {
-//     $('.formosalube-header').addClass('s-height');
-//     $('#sub-menu1').css({"top":"57px"});
-//     $('#sub-menu2').css({"top":"57px"});
-//   } else {
-//     $('.formosalube-header').removeClass('s-height');
-//     $('#sub-menu1').css({"top":"95px"});
-//     $('#sub-menu2').css({"top":"95px"});
-//   }
-// });
-
-// $(function () {
-//   $(".dropdown").hover(function(){
-//       $(this).find(".megamenu").show();
-//   },function(){
-//       $(this).find(".megamenu").hide();
-//   });
-// });
-
-$(document).ready(function() {
+const menuData = {
+  products: {
+    title: "產品",
+    sections: [
+      {
+        title: "產品系列",
+        items: [
+          {
+            name: "汽車潤滑油",
+            url: "https://www.fuchs.com/sea/tw/products/product-program/automotive-lubricants/",
+          },
+          {
+            name: "工業潤滑油",
+            url: "https://www.fuchs.com/sea/tw/products/product-program/industrial-lubricants/",
+          },
+          {
+            name: "潤滑脂",
+            url: "https://www.fuchs.com/sea/tw/products/product-program/lubricating-greases/",
+          },
+          {
+            name: "金屬加工潤滑劑",
+            url: "https://www.fuchs.com/sea/tw/products/product-program/metal-processing-lubricants/",
+          },
+          {
+            name: "特殊應用潤滑劑",
+            url: "https://www.fuchs.com/sea/tw/products/product-program/special-application-lubricants/",
+          },
+          {
+            name: "服務",
+            url: "https://www.fuchs.com/sea/tw/products/product-program/services/",
+          },
+        ],
+      },
+      {
+        title: "服務連結",
+        items: [
+          {
+            name: "油選擇器",
+            url: "https://fuchs-eu.lubricantadvisor.com/default.aspx?lang=eng",
+          },
+          {
+            name: "產品搜尋",
+            url: "https://www.fuchs.com/sea/tw/products/service-links/product-finder/",
+          },
+          {
+            name: "產品助理",
+            url: "https://www.fuchs.com/sea/tw/products/service-links/product-assistant/",
+          },
+          {
+            name: "分銷商或經銷商搜尋",
+            url: "https://www.fuchs.com/sea/tw/products/service-links/distributor-or-dealer-search/",
+          },
+        ],
+      },
+    ],
+  },
+  industries: {
+    title: "行業",
+    sections: [
+      {
+        title: "A - D",
+        items: [
+          {
+            name: "農業和林業",
+            url: "https://www.fuchs.com/sea/tw/industries/a-d/agriculture-and-forestry/",
+          },
+          {
+            name: "汽車",
+            url: "https://www.fuchs.com/sea/tw/industries/a-d/automotive/",
+          },
+          {
+            name: "璹造",
+            url: "https://www.fuchs.com/sea/tw/industries/a-d/automotive/",
+          },
+          {
+            name: "水泥、石灰和石膏",
+            url: "https://www.fuchs.com/sea/tw/industries/a-d/cement-lime-and-gypsum/",
+          },
+          {
+            name: "工業機械",
+            url: "https://www.fuchs.com/sea/tw/industries/a-d/construction/",
+          },
+          {
+            name: "消費者和售後市場",
+            url: "https://www.fuchs.com/sea/tw/industries/a-d/consumer-and-aftermarket/",
+          },
+        ],
+      },
+      {
+        title: "F - M",
+        items: [
+          {
+            name: "食品和飲料",
+            url: "https://www.fuchs.com/sea/tw/industries/f-m/food-and-beverages/",
+          },
+          {
+            name: "鍛造",
+            url: "https://www.fuchs.com/sea/tw/industries/f-m/forging/",
+          },
+          {
+            name: "玻璃",
+            url: "https://www.fuchs.com/sea/tw/industries/f-m/glass/",
+          },
+          {
+            name: "機械和設備",
+            url: "https://www.fuchs.com/sea/tw/industries/f-m/machinery-and-equipment/",
+          },
+          {
+            name: "金屬加工",
+            url: "https://www.fuchs.com/sea/tw/industries/f-m/metal-processing/",
+          },
+          {
+            name: "採購和勘探",
+            url: "https://www.fuchs.com/sea/tw/industries/f-m/mining-and-exploration/",
+          },
+        ],
+      },
+      {
+        title: "N - Z",
+        items: [
+          {
+            name: "發電",
+            url: "https://www.fuchs.com/sea/tw/industries/n-z/power-generation/",
+          },
+          {
+            name: "鋼和金屬",
+            url: "https://www.fuchs.com/sea/tw/industries/n-z/steels-and-metals/",
+          },
+          {
+            name: "運輸",
+            url: "https://www.fuchs.com/sea/tw/industries/n-z/transport/",
+          },
+          {
+            name: "其他行業",
+            url: "https://www.fuchs.com/sea/tw/industries/n-z/other-industries/",
+          },
+        ],
+      },
+    ],
+  },
+  brands: {
+    title: "品牌故事",
+    sections: [
+      {
+        title: "A - K",
+        items: [
+          {
+            name: "AGRIFARM",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/agrifarm/",
+          },
+          {
+            name: "ANTICORIT",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/anticorit/",
+          },
+          {
+            name: "CARBAFLO",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/carbaflo/",
+          },
+          {
+            name: "CASSIDA",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/cassida/",
+          },
+          {
+            name: "CEPLATTYN",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/ceplattyn/",
+          },
+          {
+            name: "CHEMPLEX",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/chemplex/",
+          },
+          {
+            name: "ECOCOOL",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/ecocool/",
+          },
+          {
+            name: "ECOCUT",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/ecocut/",
+          },
+          {
+            name: "GEARMASTER",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/gearmaster/",
+          },
+          {
+            name: "GLEITMO",
+            url: "https://www.fuchs.com/sea/tw/brands/a-k/gleitmo/",
+          },
+        ],
+      },
+      {
+        title: "L - R",
+        items: [
+          {
+            name: "LAGERMEISTER",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/lagermeister/",
+          },
+          {
+            name: "LOCOLUB",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/locolub/",
+          },
+          {
+            name: "LUBRODAL",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/lubrodal/",
+          },
+          {
+            name: "MAINTAIN",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/maintain/",
+          },
+          {
+            name: "MOLYPAUL",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/molypaul/",
+          },
+          {
+            name: "PENTOSIN",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/pentosin/",
+          },
+          {
+            name: "PLANTO",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/planto/",
+          },
+          {
+            name: "RENEP",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/renep/",
+          },
+          {
+            name: "RENISO",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/reniso/",
+          },
+          {
+            name: "RENOCLEAN",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/renoclean/",
+          },
+          {
+            name: "RENOFORM",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/renoform/",
+          },
+          {
+            name: "RENOLIN",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/renolin/",
+          },
+          {
+            name: "RENOLIT",
+            url: "https://www.fuchs.com/sea/tw/brands/l-r/renolit/",
+          },
+        ],
+      },
+      {
+        title: "S - Z",
+        items: [
+          {
+            name: "SILKOLENE",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/silkolene/",
+          },
+          { name: "SOK", url: "https://www.fuchs.com/sea/tw/brands/s-z/sok/" },
+          {
+            name: "SOLCENIC",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/solcenic/",
+          },
+          {
+            name: "STABYL",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/stabyl/",
+          },
+          {
+            name: "STABYLAN",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/stabylan/",
+          },
+          {
+            name: "THERMISOL",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/thermisol/",
+          },
+          {
+            name: "TITAN",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/titan/",
+          },
+          {
+            name: "TRENOIL",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/trenoil/",
+          },
+          {
+            name: "URETHYN",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/urethyn/",
+          },
+          {
+            name: "VITROLIS",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/vitrolis/",
+          },
+          {
+            name: "WISURA",
+            url: "https://www.fuchs.com/sea/tw/brands/s-z/wisura/",
+          },
+        ],
+      },
+    ],
+  },
+};
+$(document).ready(function () {
   // 點擊 tab 按鈕切換 tab
-  $('.tab-button').click(function() {
-    var tabId = $(this).data('tab');
-    
-  // 移除所有 tab 按鈕的 active 樣式
-  $('.tab-button').removeClass('active');
-    
-  // 添加當前點擊的 tab 按鈕 active 樣式
-  $(this).addClass('active');
-    
-  // 隱藏所有 tab 內容
-  $('.tab-content').removeClass('active');
-  
-  // 顯示點擊的 tab 內容
-  $('#' + tabId).addClass('active');
+  $(".tab-button").click(function () {
+    var tabId = $(this).data("tab");
+
+    // 移除所有 tab 按鈕的 active 樣式
+    $(".tab-button").removeClass("active");
+
+    // 添加當前點擊的 tab 按鈕 active 樣式
+    $(this).addClass("active");
+
+    // 隱藏所有 tab 內容
+    $(".tab-content").removeClass("active");
+
+    // 顯示點擊的 tab 內容
+    $("#" + tabId).addClass("active");
   });
+
+  // 載入選單資料
+  loadMenuData();
 });
+
+// 從 JSON 檔案載入選單資料
+function loadMenuData() {
+  // 產品選單
+  if (menuData.products) {
+    buildMenu("#menu-products", menuData.products);
+  }
+  // 行業選單
+  if (menuData.industries) {
+    buildMenu("#menu-industries", menuData.industries);
+  }
+  // 品牌故事選單
+  if (menuData.brands) {
+    buildMenu("#menu-brands", menuData.brands);
+  }
+}
+
+// 建立選單 HTML
+function buildMenu(selector, menuData) {
+  var $container = $(selector);
+  if ($container.length === 0) return;
+
+  var html = '<li><div class="container"><div class="row">';
+
+  menuData.sections.forEach(function (section, index) {
+    var titleClass =
+      index === 0 ? "dropdown-title bottom10" : "dropdown-title opacity-10";
+    html += '<div class="col-lg-3 col-md-6 col-sm-12 mengmenu_border">';
+    html += '<h5 class="' + titleClass + '">' + section.title + "</h5>";
+    html += "<ul>";
+
+    section.items.forEach(function (item) {
+      html +=
+        '<li><a class="dropdown-item" target="_blank" href="' +
+        item.url +
+        '">' +
+        item.name +
+        "</a></li>";
+    });
+
+    html += "</ul></div>";
+  });
+
+  html += "</div></div></li>";
+
+  $container.html(html);
+}
