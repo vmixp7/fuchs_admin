a068866a60b61a8e79bad08dfc3791523242805e
p7 <vmixp7@gmail.com>
2026-01-24 17:53:59 +0800

    add go_back

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -91,7 +91,7 @@ const EmailController = {
       // 郵件選項
       const mailOptions = {
         from: process.env.EMAIL_USER,
-        to: "vmixp7@gmail.com",
+        to: "vcnt.chang@gmail.com",
         subject: subjectName,
         text: mailContent,
       };
